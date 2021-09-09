bba8fd879e13d4d05ebcee4ba4330ec0fea6a9a2
VladSuciu <vladsuciu26@yahoo.com>
2021-09-09 20:46:30 +0300

    Commit test

--- src/ro/sci/Main.java
+++ src/ro/sci/Main.java
@@ -1,5 +1,9 @@
+package ro.sci;
+
 public class Main {
     public static void main(String[] args) {
+        // Am adaugat tema 1
 
+        // Write something
     }
 }
---
+++ src/ro/sci/team1/BMI.java
@@ -0,0 +1,4 @@
+package ro.sci.team1;
+
+public class BMI {
+}
